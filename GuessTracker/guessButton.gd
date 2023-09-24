d865ea27f49ec41b70b45e34a3e8e05d49945627
azinneck0485 <123660683+azinneck0485@users.noreply.github.com>
2023-09-24 17:27:32 -0400

    improved display using dynamic configuration of button styles

--- GuessTracker/guessButton.gd
+++ GuessTracker/guessButton.gd
@@ -1,8 +1,19 @@
 extends Button
 
+# define styleboxes here for now to enable easier adjustmest through constants later
+var stylebox_active = get_theme_stylebox("normal")
+var stylebox_hidden = get_theme_stylebox("pressed")
+var stylebox_disabled = get_theme_stylebox("disabled")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# set the styles
+	# adapted from https://www.reddit.com/r/godot/comments/12zh2qq/godot_40_why_wont_my_ui_panel_stylebox_overwrite/
+	# TODO - create constants file
+	set_style(stylebox_active, Color.DARK_GREEN, Color.BLACK)
+	set_style(stylebox_hidden, Color(1,1,1,0), Color(1,1,1,0))
+	set_style(stylebox_disabled, Color.DARK_SEA_GREEN, Color.DIM_GRAY)
+		
 	reset_button()
 
 
@@ -13,6 +24,26 @@ func _process(delta):
 func reset_button():
 	disabled = false
 
+func set_style(style, bg_color, bdr_color):
+	# styles are differentated by colors
+	style.bg_color = bg_color
+	style.border_color = bdr_color
+	
+	# outline shape is the same for all buttons
+	style.border_width_top = 3
+	style.border_width_bottom = 3
+	style.border_width_left = 3
+	style.border_width_right = 3
+	style.corner_radius_bottom_left = 5
+	style.corner_radius_bottom_right = 5
+	style.corner_radius_top_left = 5
+	style.corner_radius_top_right = 5
+
+func hide_button():
+	toggle_mode = true  # needed to set the state of the button
+	set_pressed_no_signal(true)  # per tool tip, this is used to set the state without emitting signal
+	
+	button_mask = 0  # prevent the hidden button from responding to stray clicks
 
 func _on_pressed():
 	print(text)
--- GuessTracker/guess_tracker.gd
+++ GuessTracker/guess_tracker.gd
@@ -7,8 +7,6 @@ func _ready():
 	
 	for c in get_children():
 		var start_ascii = c.name.unicode_at(0)  # per comment at https://ask.godotengine.org/106152/convert-an-character-to-ascii-value
-		print(start_ascii)
-		print(char(start_ascii))
 		
 		# TODO - add applicaiton of styleboxes
 		if start_ascii < "U".unicode_at(0):
@@ -18,12 +16,11 @@ func _ready():
 				button_i.text = char(start_ascii + i)
 				c.add_child(button_i)
 		else:  # some special things for the last column to keep it symmetric
-			print(start_ascii)
 			for i in range(-2,8):
 				var button_i = button.instantiate()
 				
 				if i < 0 or i > 5:
-					button_i.text = ""
+					button_i.hide_button()
 				else:
 					button_i.text = char(start_ascii + i)
 				
